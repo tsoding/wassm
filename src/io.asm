87218970010c1b2a9a66e24818ff60a4ac913227
rexim <reximkut@gmail.com>
2017-09-28 01:34:28 +0700

    Add TODOs for the rest of the work (#13)

--- src/io.asm
+++ src/io.asm
@@ -6,6 +6,10 @@ hello_world:    db "read_line_fd", 10, 0
     SECTION .text
     global read_line_fd
 read_line_fd:
+    ;; TODO: implement read_line_fd
+    ;;
+    ;; This function takes a file descriptor and returns next line from it.
+    ;; The returned string should freed manually
     mov rdi, hello_world
     mov rax, 0
     call printf
