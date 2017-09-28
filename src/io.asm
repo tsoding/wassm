bc298b35da4a71d439abd07017724226ff0e071c
rexim <reximkut@gmail.com>
2017-09-28 15:07:57 +0700

    Link TODO(#26)

--- src/io.asm
+++ src/io.asm
@@ -6,7 +6,7 @@ hello_world:    db "read_line_fd", 10, 0
     SECTION .text
     global read_line_fd
 read_line_fd:
-    ;; TODO: implement read_line_fd
+    ;; TODO(#26): implement read_line_fd
     ;;
     ;; This function takes a file descriptor and returns next line from it.
     ;; The returned string should freed manually
