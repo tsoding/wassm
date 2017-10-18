e7894c6b256cc752f8f342495533157ab1eedb75
rexim <reximkut@gmail.com>
2017-10-19 01:43:37 +0700

    Add TODOs for HTTP parsing

--- src/http.asm
+++ src/http.asm
@@ -6,11 +6,14 @@
     global parse_method
     global parse_request_uri
 drop_sp:
+    ;; TODO: implement drop_sp function
     mov rax, rdi
     ret
 parse_method:
+    ;; TODO: implement parse_method function
     mov rax, rdi
     ret
 parse_request_uri:
+    ;; TODO: implement parse_request_uri function
     mov rax, rdi
     ret
