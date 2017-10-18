e9cc89de56214d5c9f42bff0162efb4b7e1256a3
rexim <reximkut@gmail.com>
2017-10-19 01:42:47 +0700

    Remove obsolete route_from_line function

--- src/http.asm
+++ src/http.asm
@@ -1,21 +1,10 @@
     ;; -*- mode: asm -*-
     SECTION .data
-route:
-    db "/", 0
     SECTION .text
     global route_from_line
     global drop_sp
     global parse_method
     global parse_request_uri
-    ;; route_from_line should deallocate input string
-route_from_line:
-    ;; TODO(#27): Implement route_from_line
-    ;;
-    ;; This function takes a string that represents a status line of an HTTP request and
-    ;; returns the route of the request. It also frees the input string. The output string should be
-    ;; freed by the caller
-    mov rax, route
-    ret
 drop_sp:
     mov rax, rdi
     ret
