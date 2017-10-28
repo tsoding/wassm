bb7664f419d1bea5e9cdd7fcc4efde273546788a
rexim <reximkut@gmail.com>
2017-10-29 01:40:37 +0700

    More precise implementation of http::is_uri_char (#40)

--- src/http.asm
+++ src/http.asm
@@ -1,18 +1,33 @@
 %include "c.inc"
 
 ;;; -*- mode: nasm -*-
-    SECTION .data
-    SECTION .text
-    global drop_sp
-    global parse_method
-    global parse_request_uri
+SECTION .data
+
+non_alnum_uri_chr:
+    db "/?=&", 0
+
+SECTION .text
+global drop_sp
+global parse_method
+global parse_request_uri
 
 is_uri_char:
-    mov rax, 1
-    cmp rdi, 0x20
+    push rbp
+    mov rbp, rsp
+    sub rsp, 16
+    mov [rbp - 8], rdi
+
+    call isalnum
+    cmp rax, 0
     jne .end
-    mov rax, 0
+
+    mov rdi, non_alnum_uri_chr
+    mov rsi, [rbp - 8]
+    call strchr
+
 .end:
+    mov rsp, rbp
+    pop rbp
     ret
 
 drop_sp:
